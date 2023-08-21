619d09bc278dd4c6ec81ddcb33d2025c61548abc
Vivi988 <112839375+Vivi988@users.noreply.github.com>
2023-08-21 15:27:19 +1100

    Commentaire Etapes pour game.php

---
+++ Game/game.php
@@ -0,0 +1,32 @@
+<?php
+
+###  START GAME INITIALISATION
+//init variables de sessions :
+    //VieJoueur
+//init generationRandomMobs
+
+
+
+#position_player == position_mob => FIGHT
+###  FIGHT
+
+
+
+## PLAYER WIN 
+
+# HP RESET
+
+# GAIN XP = PV MOB
+
+
+# PLAYER LOOSE = RESTART GAME
+# HP =< 0 
+
+
+
+
+
+
+
+
+?>
