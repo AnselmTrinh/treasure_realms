caead7b1ff184971c095507c5ff24b874e0400cf
Yohann <yohann-devweb3@github.com>
2023-08-22 08:47:05 +1100

    function move() added

--- player.php
+++ player.php
@@ -4,5 +4,25 @@ class Player extends Characters {
   public function message() {
     echo "Je ne faiblirai pas devant toi !";
   }
+  public function move($keyPress) {
+switch ($keyPress) {
+    case 'UP':
+        echo "le joueur se dirige en haut";
+        break;
+    case 'DOWN':
+        echo "le joueur se dirige en bas";
+        break;
+    case 'LEFT':
+        echo "le joueur se dirige à gauche";
+    case 'RIGHT':
+        echo "le joueur se dirige à droite";
+    break;
+    default:
+        # code...
+        break;
+}
+
+    echo "le joueur avance";
+  }
 }
 ?>
